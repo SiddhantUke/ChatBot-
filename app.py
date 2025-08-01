08cfb587b33c07fa9ca9c1f1dbec17033843f680
Siddhant Uke <ukesidd@gmail.com>
2025-08-02 00:13:25 +0530

    A Text-to-Math Problem Solver & Knowledge Assistant Powered by Gemma 2!

--- app.py
+++ app.py
@@ -1,103 +1,127 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
-from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
-from langchain.agents import initialize_agent,AgentType
-from langchain.callbacks import StreamlitCallbackHandler ## Communicate  all tools within themselves.
-import os 
-from dotenv import load_dotenv
-
-# DuckDuckGoSearchRun --------> For search from an internet !
+from langchain.chains import LLMMathChain, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.agents.agent_types import AgentType
+from langchain.agents import Tool, initialize_agent
+from langchain.callbacks import StreamlitCallbackHandler
 
 
-
-## INITIALIZE THE TOOLS THAT IS ARXIV AND WIKIPEDIA TOOLS 
-
-arxiv_wrapper = ArxivAPIWrapper(top_k_result=1, doc_content_chars_max=200)
-arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
-
-api_wrapper = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
-wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
-
-## The Kind of Task that we want to do from duck duck go search !
-
-search=DuckDuckGoSearchRun(name="Search")
+## from dotenv import load_dotenv
+# load_dotenv()
+## IF WE ARE USING ENVIRONMENT VARIABLES 
+## GIVING THE API KEY IN THE .env file
+## WE ARE GIVING THE API KEY FROM THE WEBSITE DIRECTLY
 
 
-## Title 
+## SET UP THE STREAMLIT APP
+st.set_page_config(page_title="Text To Math Problem Solver And Data Search Assistant", page_icon="🧮")
+st.title("Text To Math Problem Solver Using Google Gemma 2")
 
-st.title("🔎 LangChain - Chat-Agent")
+## WHICH LLM MODEL WE ARE USING ?
+## WE ARE USING GEMMA 2 MODEL FROM GROQ
+
+groq_api_key = st.sidebar.text_input(label="Groq API Key", type="password")
+
+if not groq_api_key:
+    st.info("Please add your Groq API key to continue")
+    st.stop()
+    
+llm = ChatGroq(model="Gemma2-9b-It", groq_api_key=groq_api_key)
+
+## INITIALIZING THE TOOLS
+
+wikipedia_wrapper = WikipediaAPIWrapper()
+wikipedia_tool = Tool(
+    name="Wikipedia",
+    func=wikipedia_wrapper.run,
+    description="A tool for searching the Internet to find various information on the topics mentioned"
+)
+    
+## If we want to run the app then we need to do the functn that is wikipedia_wrapper.run
+
+## INITIALIZE THE MATH TOOL
+
+math_chain = LLMMathChain.from_llm(llm=llm)
+calculator = Tool(
+    name="Calculator",
+    func=math_chain.run,
+    description="A tool for answering math related questions. Only input mathematical expression need to be provided"
+)
+
+prompt = """Your an agent tasked for solving users mathematical question. Logically arrive at the solution and provide a detailed explanation
+and display it point wise for the question below
+Question: {question}
+Answer:
 """
-Meet the chatbot that doesn’t just respond — it investigates.
 
-Built with LangChain and Groq’s lightning-fast LLM, this assistant intelligently decides when to search the live web, when to pull academic insights from arXiv, 
-and when to explore Wikipedia — all while revealing its thought process in real time through a dynamic Streamlit interface.
-"""
+prompt_template = PromptTemplate(
+    input_variables=["question"],
+    template=prompt
+)
+
+## COMBINE ALL THE TOOLS INTO CHAIN
+
+## Before that we need to  create a llm chain 
+
+chain = LLMChain(llm=llm, prompt=prompt_template)
+    ## we need to create a chain for reasoning
+    ## Reasoning tool for answering logic-based and reasoning questions
+    
+reasoning_tool = Tool(
+    name="Reasoning tool",
+    func=chain.run,
+    description="A tool for answering logic-based and reasoning questions."
+)
 
 
-## Help us to what action and agent that actually work with it ! or when it is interacting with the tools.
+## INITIALIZE THE AGENTS
 
-
-
-
-## Creating side bar for calling any api ! for 
-## Sidebar for setting
-
-st.sidebar.title("Settings")
-api_key=st.sidebar.text_input("Enter your Groq API Key:",type="password")
-
-
-
-## So now my entire conversation happend along with the chat history ! 
-
+assistant_agent = initialize_agent(
+    tools=[wikipedia_tool, calculator, reasoning_tool],
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False,
+    handle_parsing_errors=True
+)
 
 if "messages" not in st.session_state:
-    st.session_state["messages"]=[
-        {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web."}  ## Default assisstant information that we have ! 
+    st.session_state["messages"] = [
+        {"role": "system", "content": "You are a helpful assistant that can answer math questions and search the internet for information."}
     ]
-
-
+    
+    
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg['content'])
     
     
-if prompt:=st.chat_input(placeholder="How can i help you ?"):
-    st.session_state.messages.append({"role":"user","content":prompt})
-    st.chat_message("user").write(prompt)
-    
-    ## Inside this  groq we have to create llm 
-    
-    llm = ChatGroq(groq_api_key = api_key,model_name = "Llama3-8b-8192" , streaming=True)
-    ## We have to take this groq api key with a parameter with a input itself.
-    
-    ## We have to initialize the tools ------> wiki,arxiv and search !
-    tools = [search,arxiv,wiki]
-    
-    ## We have to convert that  entire tools into an agent so we able to invoke this 
-    
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
-    
-    ##ZERO_SHOT_REACT_DESCRIPTION AND CHAT_ZERO_SHOT_REACT_DESCRIPTION (CHECK DOC )
-    
-    # basically lies in how they handle context and memory and how they generate prompt from the language model 
-    
-    ######## ZERO_SHOT_REACT_DESCRIPTION --------------> THEY RELAY ON THE CHAT  HISTORY ! 
-    
-    ######## CHAT_ZERO_SHOT_REACT_DESCRIPTION ------------> IT USES A CHAT HISTORY TO REMEMBERED THE CONTEXT OF THE CHAT HISTORY OF THE CONVERSATION ! 
-    ######## IT ACCESS A CERTAIN STRUCTURE IN THE CHAT HISTORY MIGHT RAISED AN ERROR IF IT DOES NOT FIND IT ! 
-    ######## WE ARE USING HERE ---------------> ZERO_SHOT 
-    
-    with st.chat_message("assistant"):
-        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
-        
-        ## once we use this streamlit callback handle it is display the thoughts and action in agent in inter active streamlit app 
-        ### SO NOW WHENEVER MY AGENT IS COMMUNICATE WITH ITSELF  
-        
-        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
-        
-        st.session_state.messages.append({'role':'assistant',"content":response})
-        
-        st.write(response)
+## FUNCTION THE GENERATE THE RESPONSE
 
+def generate_response(user_question):
+    response = assistant_agent.invoke({"input": question})
+    return response
+ ## This function will take the user question and invoke the assistant agent to get the response
+ 
+## LETS START THE INTERACTION
+question=st.text_area("Enter youe question:","I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
+
+# if st.button("Find my answer"):
     
+    ## RE UNDERSTAND HERE AND AGAIN 
     
+if st.button("find my answer"):
+    if question:
+        with st.spinner("Generate response.."):
+            st.session_state.messages.append({"role":"user","content":question})
+            st.chat_message("user").write(question)
+
+            st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+            response=assistant_agent.run(st.session_state.messages,callbacks=[st_cb]
+                                         )
+            st.session_state.messages.append({'role':'assistant',"content":response})
+            st.write('### Response:')
+            st.success(response)
+
+    else:
+        st.warning("Please enter the question")
